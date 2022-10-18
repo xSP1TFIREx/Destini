c08d17b47772e01c92a08415213300e973ba5b3c
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-10-18 13:02:31 +0300

    Another day - another commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,11 +6,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
+        
+    }
+    
+    @IBAction func choiseMade(_ sender: UIButton) {
+        nextStory(sender.currentTitle!)
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        storyLabel.text = story.title[currentCounter]
+        choice1Button.setTitle(story.choice1[currentCounter], for: .normal)
+        choice2Button.setTitle(story.choice2[currentCounter], for: .normal)
 
     }
-
-
+    
 }
+
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -1,11 +1,30 @@
-//
-//  StoryBrain.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import Foundation
 
+var currentCounter = 0
 
+
+let story = Story(title: stories,
+                  choice1: choices1,
+                  choice2: choices2)
+
+
+let stories = ["You see a fork in the road.", "You see a tiger.", "You`ve found a treasure chest."]
+let choices1 = ["Take a left.", "Shout for help.", "Open it."]
+let choices2 = ["Take a right.", "Play dead.", "Check for traps."]
+
+
+func nextStory(_ currentChoice: String) {
+    if currentChoice == choices1[currentCounter] {
+        currentCounter += 1
+    } else {
+        currentCounter += 2
+    }
+    checkCurrentCounter()
+}
+
+
+func checkCurrentCounter() {
+    if currentCounter > stories.count - 1 {
+        currentCounter = 0
+    }
+}
